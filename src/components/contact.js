e3e704af4943e594cbc2484418d2a5c9d6e67a9b
Gloria Ma <gloria.ma@aya.yale.edu>
2016-03-02 09:50:19 -0800

    Adds button for highlighting email address (not functional)

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Clipboard from 'clipboard';
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    new Clipboard('.clipboardButton');
+  }
+
+  selectInputValue(target) {
+    target.setSelectionRange(0, target.value.length);
+  }
+
   render() {
     return (
       <div className="content">
-        <p>I am best reached via email at: gloria.ma@hackreactor.com [copy to clipboard]</p>
+        <p>
+          I am best reached via email at:
+          <input onClick={(e) => this.selectInputValue(e.target)} type="text" id="email-address" value="gloria.ma@hackreactor.com" readOnly />
+          <button className="clipboardButton" data-clipboard-target="email-address">
+            <img className="icon" src="assets/octicon-clippy.png" alt="Copy to clipboard" />
+          </button>
+        </p>
         
         <p>Please feel free to introduce yourself and share a challenge you are currently facing, and we'll get lunch or tea.</p>
       </div>
